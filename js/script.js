6adf56194935f73baf67992f49e7a8041d3f8d38
SmXme <pillot.clement@gmail.com>
2017-11-30 11:34:18 +0100

    Added functions to add brands and display them in a list

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
-myProducts=[];
+// 				GLOBAL VARIABLES				  //
 
-function Products (theName,thePrice,theQty){
+myProducts=[];
+myBrands=[];
+
+
+
+// 				CLASSES				  //
+function Products (productName,productPrice,productQty){
 	this.name= theName;
 	this.price = thePrice;
 	this.qty = theQty;
 }
 
-$('#inputAddProduct').click(addProduct);
+function Brands(brandName){
+	this.name = brandName;
+	this.brandProducts = []; 
+}
 
+
+
+
+
+// 				EVENTLISTENERS				  //
+$('#inputAddProduct').click(addProduct);
+$('#inputAddBrand').click(addBrand);
+
+
+
+
+// 				FUNCTIONS				  //
 function addProduct(){
 	myProducts.push(new Products($('#inputProductName').val(),$('#inputProductPrice').val(),$('#inputProductQty').val()));
 	showProductList();
@@ -17,7 +38,18 @@ function showProductList(){
 	$('#productsList').html("");
 	for (var i = 0 ; i < myProducts.length ; i++) {
 		$('#productsList').append('<li class="productLine">'+myProducts[i].name+'</li>');
-		console.log(myProducts[i]);
 	}
-	console.log(myProducts);
+}
+
+function addBrand(){
+	myBrands.push(new Brands($('#inputBrandName').val()));
+	console.log(myBrands);
+	showBrandList();
+}
+
+function showBrandList(){
+	$('#brandsList').html("");
+	for (var i = 0 ; i < myBrands.length ; i++) {
+		$('#brandsList').append('<li class="brandLine">'+myBrands[i].name+'</li>');
+	}
 }
